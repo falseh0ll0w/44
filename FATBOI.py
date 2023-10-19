7fc1408526b10f9c6c6e4cba9ef76c33401e865e
falseh0ll0w <ryan.forrestall@gmail.com>
2023-10-19 07:57:35 -0700

    yerp

--- FATBOI.py
+++ FATBOI.py
@@ -1,110 +1,101 @@
-import logging
-import re
-from typing import List
-
 import tkinter as tk
 from tkinter import filedialog
-from sympy import Add, Mul, Pow, Symbol, cancel, checksol, divisor_count, itermonomials, motzkin, primefactors, rad, reshape, root, zeros
+from typing import Tuple
+
+
+from sympy import latex
 from sympy.parsing.sympy_parser import (
     implicit_multiplication_application,
     parse_expr,
     standard_transformations,
 )
-import pylint.lint
-from pylint.reporters.text import TextReporter
 
-logging.basicConfig(filename='code_extraction.log', level=logging.ERROR)
 
-try:
-    import os
-    from pathlib import Path
-    from typing import List, Tuple
-    from sympy import *
-    from bs4 import BeautifulSoup
-    from io import StringIO
+def pdf_to_text(file_path: str) -> str:
+    """
+    Convert PDF to text.
+
+    Args:
+        file_path: The path to the PDF file.
+
+    Returns:
+        The text extracted from the PDF file.
+    """
     import PyPDF2
-    import requests
-    import base64
 
-    logging.getLogger().setLevel(logging.INFO)
+    with open(file_path, "rb") as f:
+        pdf_reader = PyPDF2.PdfFileReader(f)
+        page = pdf_reader.getPage(0)
+        text = page.extractText()
+    return text
 
-    def extract_and_save_code_blocks(conversation: str, file_name: str) -> None:
-        code_blocks = extract_and_analyze_code_blocks(conversation)
-        save_code_blocks_to_file(code_blocks, file_name)
 
-    def extract_and_analyze_code_blocks(conversation: str) -> List[str]:
-        code_blocks = []
-        soup = BeautifulSoup(conversation, 'html.parser')
-        for code_block in soup.find_all('code'):
-            code_blocks.append(code_block.text)
-        return code_blocks
+def text_to_latex(text: str) -> str:
+    """
+    Convert text to LaTeX.
 
-    def analyze_conversation(conversation: str) -> None:
-        math_and_algorithms = []
-        soup = BeautifulSoup(conversation, 'html.parser')
-        for code_block in soup.find_all('code'):
-            if 'import' in code_block.text:
-                exec(code_block.text)
-            else:
-                math_and_algorithms.append(code_block.text)
-        for algorithm in math_and_algorithms:
-            exec(algorithm)
+    Args:
+        text: The text to convert.
 
-    def save_code_blocks_to_file(code_blocks: List[str], file_name: str) -> None:
-        with open(file_name, 'w') as f:
-            for code_block in code_blocks:
-                f.write(code_block + '\n')
+    Returns:
+        The LaTeX representation of the text.
+    """
+    # Apply standard transformations to parse text as mathematical expressions
+    transformations = standard_transformations + (implicit_multiplication_application,)
+    expr = parse_expr(text, transformations=transformations)
+    # Convert expression to LaTeX
+    return latex(expr)
 
-    def mainloop() -> None:
-        root = tk.Tk()
-        root.withdraw()
-        file_name_label = tk.Label(root, text="File Name:")
-        file_name_label.grid(row=8, column=0, sticky='w')
-        file_name_entry = tk.Entry(root, width=20)
-        file_name_entry.insert(0, "code_blocks.txt")
-        file_name_entry.grid(row=9, column=0, padx=5, pady=5)
 
-        def extract_and_save_code_blocks_wrapper() -> None:
-            file_name = file_name_entry.get()
-            file_path = filedialog.askopenfilename()
-            if file_path:
-                with open(file_path, 'r') as f:
-                    conversation = f.read()
-                extract_and_save_code_blocks(conversation, file_name)
-                analyze_conversation(conversation)
-                status_label.config(text='Code blocks extracted and analyzed successfully.')
-            else:
-                status_label.config(text='No file selected.')
+def pdf_to_latex(file_path: str) -> str:
+    """
+    Convert PDF to LaTeX.
 
-        # Extract Code Blocks Button
-        extract_code_blocks_button = tk.Button(root, text="Extract Code Blocks", command=extract_and_save_code_blocks_wrapper)
-        extract_code_blocks_button.grid(row=10, column=0, padx=5, pady=5)
+    Args:
+        file_path: The path to the PDF file.
 
-        # Extract and Analyze Code Blocks Button
-        extract_and_analyze_code_blocks_button = tk.Button(root, text="Extract and Analyze Code Blocks", command=lambda: extract_and_save_code_blocks_wrapper())
-        extract_and_analyze_code_blocks_button.grid(row=11, column=0, padx=5, pady=5)
+    Returns:
+        The LaTeX representation of the text extracted from the PDF file.
+    """
+    text = pdf_to_text(file_path)
+    return text_to_latex(text)
 
-        # Analyze Conversation Button
-        analyze_conversation_button = tk.Button(root, text="Analyze Conversation", command=lambda: analyze_conversation_wrapper())
-        analyze_conversation_button.grid(row=12, column=0, padx=5, pady=5)
 
-        def analyze_conversation_wrapper() -> None:
-            file_path = filedialog.askopenfilename()
-            if file_path:
-                with open(file_path, 'r') as f:
-                    conversation = f.read()
-                analyze_conversation(conversation)
-                status_label.config(text='Conversation analyzed successfully.')
-            else:
-                status_label.config(text='No file selected.')
+class Application(tk.Frame):
+    def __init__(self, master: tk.Tk) -> None:
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-        # Status Label
-        status_label = tk.Label(root, text="")
-        status_label.grid(row=13, column=0, padx=5, pady=5)
+    def create_widgets(self) -> None:
+        self.select_file_button = tk.Button(
+            self, text="Select PDF file", command=self.select_file
+        )
+        self.select_file_button.pack(side="top", padx=10, pady=10)
 
-        root.mainloop()
+        self.convert_button = tk.Button(
+            self, text="Convert to LaTeX", command=self.convert_to_latex
+        )
+        self.convert_button.pack(side="top", padx=10, pady=10)
 
-except ImportError as e:
-    logging.error(f"ImportError: {e}")
-except Exception as e:
-    logging.error(f"Exception: {e}")
+        self.quit_button = tk.Button(
+            self, text="Quit", fg="red", command=self.master.destroy
+        )
+        self.quit_button.pack(side="bottom", padx=10, pady=10)
+
+        self.file_path_label = tk.Label(self, text="")
+        self.file_path_label.pack(side="top", padx=10, pady=10)
+
+        self.latex_output_label = tk.Label(self, text="")
+        self.latex_output_label.pack(side="top", padx=10, pady=10)
+
+    def select_file(self) -> None:
+        file_path = filedialog.askopenfilename()
+        self.file_path_label.config(text=file_path)
+
+    def convert_to_latex(self) -> None:
+        file_path = self.file_path_label.cget("text")
+        if file_path:
+            latex_output = pdf_to_latex(file_path)
+            self.latex_output_label.config(text=latex_output)
--- Library_sorter.py
+++ Library_sorter.py
@@ -5,6 +5,16 @@ from PIL import Image, ImageDraw, ImageFont
 import win32clipboard
 
 
+def sort_by_selection(self, selection):
+    """
+    Sorts all files in the source folder based on the given selection.
+
+    Args:
+        selection (str): The selection to sort the files by.
+
+    Returns:
+        None
+    """
 class LibrarySorter:
     def __init__(self, source_folder):
         self.source_folder = source_folder
@@ -20,161 +30,78 @@ class LibrarySorter:
 
     def sort_by_selection(self, selection):
         """Sorts all files in the source folder based on the given selection."""
-        root_destination_folder = os.path.join(self.source_folder, selection)
-        os.makedirs(root_destination_folder, exist_ok=True)
+        import os
+        import subprocess
+        import shutil
+        from typing import List, Tuple
 
-        all_files = self._get_all_files()
 
-        if selection == "sort44":
-            self._sort_44(all_files, root_destination_folder)
-        else:
-            self._sort_by_file_type(all_files, root_destination_folder)
+        class ImageProcessor:
+            def __init__(self, source_folder: str):
+                self.source_folder = source_folder
 
-    def export_to_folder(self, destination_folder):
-        """Exports the sorted files and folders to the destination folder."""
-        shutil.copytree(self.source_folder, destination_folder)
+            def __repr__(self) -> str:
+                return f"ImageProcessor({self.source_folder})"
 
-    def combine_python_code(self, output_file, *input_files):
-        """Combines Python code from input files into the output file."""
-        combined_code = ''
-        for input_file in input_files:
-            with open(input_file, 'r') as file:
-                code = file.read()
-                combined_code += code + '\n\n'
+            def _get_files(self, selection: List[str]) -> List[str]:
+                files = []
+                for root, _, filenames in os.walk(self.source_folder):
+                    for filename in filenames:
+                        if filename.endswith(tuple(selection)):
+                            files.append(os.path.join(root, filename))
+                return files
 
-        with open(output_file, 'w') as file:
-            file.write(combined_code)
+            def resize_images(self, destination_folder: str, output_size: Tuple[int, int]) -> None:
+                for file_path in self._get_files([".jpg", ".jpeg", ".png"]):
+                    img = Image.open(file_path)
+                    img = img.resize(output_size, Image.ANTIALIAS)
+                    output_file = os.path.join(destination_folder, os.path.basename(file_path))
+                    img.save(output_file)
 
-    def scrape_pdf_python_code(self, file_path):
-        """Scrapes Python code from a PDF file."""
-        with open(file_path, 'rb') as file:
-            pdf = PdfFileReader(file)
-            code_text = ''
-            for page_num in range(pdf.getNumPages()):
-                page = pdf.getPage(page_num)
-                text = page.extractText()
-                code_text += self._extract_python_code_from_text(text)
-            return code_text
+            def convert_images(self, destination_folder: str, output_format: str) -> None:
+                for file_path in self._get_files([".jpg", ".jpeg", ".png"]):
+                    img = Image.open(file_path)
+                    output_file = os.path.join(destination_folder, os.path.splitext(os.path.basename(file_path))[0] + f".{output_format}")
+                    img.save(output_file)
 
-    def display_results_popup(self, window_text, x, y, width, height):
-        """Displays the text in a results popup window with custom dimensions."""
-        with open('results.txt', 'w') as file:
-            file.write(window_text)
-        image = Image.new('RGB', (width, height), (255, 255, 255))
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype('arial.ttf', size=12)
-        draw.text((10, 10), window_text, font=font, fill=(0, 0, 0))
-        image.save('results.png')
-        cropped_image = image.crop((x, y, x + width, y + height))
-        cropped_image.show()
+            def crop_images(self, output_folder: str, selection: List[str], x: int, y: int, width: int, height: int) -> None:
+                for file_path in self._get_files(selection):
+                    img = Image.open(file_path)
+                    img = img.crop((x, y, x + width, y + height))
+                    output_file = os.path.join(output_folder, os.path.basename(file_path))
+                    img.save(output_file)
 
-    def hex_code_preview_tool(self):
-        """Displays a color preview tool with the provided color palette."""
-        tool_width = 400
-        tool_height = 300
-        tool = Image.new('RGB', (tool_width, tool_height), (255, 255, 255))
+            def draw_drag_box(self, window_text: str, x: int, y: int, width: int, height: int) -> None:
+                subprocess.Popen(["nircmd.exe", "win", "activate", "class", "Shell_TrayWnd", "setsize", str(x), str(y), str(width), str(height)])
 
-        preview_box_width = (tool_width - 40) // 5
-        preview_box_height = (tool_height - 60) // 5
-        x_start = 20
-        y_start = 20
+            def copy_files(self, files: List[str], root_destination_folder: str) -> None:
+                for file_path in files:
+                    destination_folder = os.path.join(root_destination_folder, os.path.dirname(file_path))
+                    os.makedirs(destination_folder, exist_ok=True)
+                    shutil.copy(file_path, destination_folder)
 
-        for i, color_hex in enumerate(self.theme_colors):
-            row = i // 5
-            col = i % 5
-            x = x_start + (col * (preview_box_width + 10))
-            y = y_start + (row * (preview_box_height + 10))
-            color_rgb = tuple(int(color_hex[i:i + 2], 16) for i in (0, 2, 4))
-            draw = ImageDraw.Draw(tool)
-            draw.rectangle([(x, y), (x + preview_box_width, y + preview_box_height)], fill=color_rgb)
+            def move_files(self, files: List[str], root_destination_folder: str) -> None:
+                for file_path in files:
+                    destination_folder = os.path.join(root_destination_folder, os.path.dirname(file_path))
+                    os.makedirs(destination_folder, exist_ok=True)
+                    shutil.move(file_path, destination_folder)
 
-        drag_box_x = 200
-        drag_box_y = 200
-        drag_box_width = 100
-        drag_box_height = 100
-        drag_box_color = tuple(int('e5989b'[i:i + 2], 16) for i in (0, 2, 4))
-        draw_drag_box(tool, drag_box_x, drag_box_y, drag_box_width, drag_box_height, drag_box_color)
+            def rename_files(self, text: str, selection: List[str]) -> None:
+                for file_path in self._get_files(selection):
+                    file_name, file_ext = os.path.splitext(os.path.basename(file_path))
+                    new_file_name = f"{text}{file_ext}"
+                    new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
+                    os.rename(file_path, new_file_path)
 
-        tool.show()
+            def create_folder(self, folder_path: str) -> None:
+                os.makedirs(folder_path, exist_ok=True)
 
-    def toggle_copy(self, selected_colors):
-        """Copies the hex codes of selected colors to the clipboard."""
-        hex_codes = [self.theme_colors[color_index] for color_index in selected_colors]
+            @staticmethod
+            def run_script(script_path: str) -> None:
+                subprocess.Popen(["python", script_path])
 
-        hex_codes_text = '\n'.join(hex_codes)
-        win32clipboard.OpenClipboard()
-        win32clipboard.EmptyClipboard()
-        win32clipboard.SetClipboardText(hex_codes_text)
-        win32clipboard.CloseClipboard()
-
-    def _get_all_files(self):
-        """Recursively retrieves all files in the source folder."""
-        all_files = []
-        for root, dirs, files in os.walk(self.source_folder):
-            for file in files:
-                file_path = os.path.join(root, file)
-                all_files.append(file_path)
-        return all_files
-
-    def _sort_44(self, files, root_destination_folder):
-        """Sorts files using 'sort44' selection."""
-        for file_path in files:
-            filename = os.path.basename(file_path)
-            _, file_extension = os.path.splitext(filename)
-
-            if file_extension in self.script_extensions:
-                destination_folder = os.path.join(root_destination_folder, 'scripts')
-            elif file_extension in self.library_extensions:
-                destination_folder = os.path.join(root_destination_folder, 'libraries')
-            elif file_extension in self.data_extensions:
-                destination_folder = os.path.join(root_destination_folder, 'data')
-            elif file_extension in self.document_extensions:
-                destination_folder = os.path.join(root_destination_folder, 'documents')
-            elif any(module_name in file_path for module_name in self.module_names):
-                destination_folder = os.path.join(root_destination_folder, 'modules')
-            else:
-                destination_folder = os.path.join(root_destination_folder, 'other_files')
-
-            os.makedirs(destination_folder, exist_ok=True)
-            shutil.move(file_path, os.path.join(destination_folder, filename))
-
-    def _sort_by_file_type(self, files, root_destination_folder):
-        """Sorts files based on their file type."""
-        for file_path in files:
-            filename = os.path.basename(file_path)
-            _, file_extension = os.path.splitext(filename)
-
-            destination_folder = os.path.join(root_destination_folder, file_extension[1:])
-            os.makedirs(destination_folder, exist_ok=True)
-            shutil.move(file_path, os.path.join(destination_folder, filename))
-
-    def _extract_python_code_from_text(self, text):
-        """Extracts Python code blocks from text based on code block markers."""
-        lines = text.split('\n')
-        code_blocks = []
-        current_block = []
-        in_code_block = False
-
-        for line in lines:
-            if any(marker in line for marker in self.code_block_markers):
-                in_code_block = not in_code_block
-
-                if not in_code_block:
-                    code_blocks.append('\n'.join(current_block))
-                    current_block = []
-            elif in_code_block:
-                current_block.append(line.strip())
-
-        return '\n'.join(code_blocks)
-
-    def create_folder(self, folder_path):
-        """Creates a folder at the given path."""
-        os.makedirs(folder_path, exist_ok=True)
-        print(f"Created folder: {folder_path}")
-
-    def execute_script(self, script_path):
-        """Executes a Python script from the given script path."""
-        if os.path.isfile(script_path) and script_path.endswith('.py'):
+            def compress_images(self, output_file: str, *input_files: str) -> None:
+                subprocess.run(["tar", "-czvf", output_file] + list(input_files), check=True)
             os.system(f'python {script_path}')
             print(f"Executed script: {script_path}")
         else:
